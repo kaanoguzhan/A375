b975092f97aca396c7f6be12aa83f45166837dbd
Kaan Oguzhan <kaan.oguzhan>
2025-02-12 14:42:00 +0100

    dtype

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ prompts = [
 ]
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
-llm = LLM(model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
+llm = LLM(model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", dtype="half")
 
 outputs = llm.generate(prompts, sampling_params)
 
